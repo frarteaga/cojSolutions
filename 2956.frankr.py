78f5f2d70465206b208ff124d7a455b614d7d7c0
Frank Arteaga Salgado <farteaga@ipvce.lt.rimed.cu>
2016-04-24 01:20:07 -0400

    Memoizando factoriales
    	modified:   2956.frankr.py

--- 2956.frankr.py
+++ 2956.frankr.py
@@ -3,15 +3,32 @@
 #frankr@coj
 
 import math
+import functools
 
 fact = math.factorial
 
+def memoize(function):
+    cache = {}
+    @functools.wraps(function)
+    def memoized(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            res = function(*args)
+            cache[args] = res
+            return res
+    return memoized
+
 MOD = 10 ** 9 + 7
+
+@memoize
+def f(x):
+	return fact(x)
 
 while 1:
 	try:
 		N, K = [int(x) for x in raw_input().split()]
-		res = fact(N) / (fact(N / K) ** K) % MOD
+		res = f(N) / (f(N / K) ** K) % MOD
 		print res
 	except:
 		break
