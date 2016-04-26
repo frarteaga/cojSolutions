d90dfa4e94f49727dc5df0338963ed140c3594db
Frank Arteaga Salgado <farteaga@ipvce.lt.rimed.cu>
2016-04-26 10:19:21 -0400

    Plan de trabajo restante
    	new file:   map.txt

--- TBA/challenge_G25ry3CHK6SLnerwE.cpp
+++ TBA/challenge_G25ry3CHK6SLnerwE.cpp
@@ -1,20 +1,7 @@
 /*
-Given a positive integer n, find the number of pairs (i, j) in the set {0, 1, 2, ... 2^n - 1}, such that j > i and j & i == i.
-
-Example
-
-For n = 2, the output should be
-countPairs(n) = "5".
-
-The valid pairs are: (0, 1), (0, 2), (0, 3), (1, 3) and (2, 3).
-
-    [input] integer n
-
-	    1 ≤ n ≤ 30.
-
-    [output] string
-
-	    String representation of the number of pairs.
+Contando pares ST1
+Fuerza bruta: iterar por todos lo pares y comprobar la condición
+frankr@coj
 */
 
 #include <bits/stdc++.h> 
@@ -22,14 +9,17 @@ The valid pairs are: (0, 1), (0, 2), (0, 3), (1, 3) and (2, 3).
 using namespace std;
 
 int main(){
-	for (int n = 1 ; n <= 15 ; n++){
+	int n;
+	cin >> n;
+	//for (int n = 1 ; n <= 15 ; n++){
 		int c = 0;
 		for (int i = 0 ; i < (1 << n) ; i++)
 			for (int j = i + 1 ; j < (1 << n) ; j++)
 				if ((j & i) == i)
 					c++;
-		cout << n << ") " << c << endl;
-	}
+		cout << c << '\n';
+//		cout << n << ") " << c << endl;
+//	}
 
 	return 0;
 }
