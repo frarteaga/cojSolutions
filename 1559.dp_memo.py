af85a27a1aa841468a66d67e8dc51e1435765cf6
Frank Arteaga Salgado <farteaga@ipvce.lt.rimed.cu>
2016-05-19 16:09:32 -0400

    dp memo slow TLE 	modified:   1559.dp_memo.py

--- 1559.dp_memo.py
+++ 1559.dp_memo.py
@@ -22,12 +22,14 @@ def memoize(function):
 N = M = K = 0
 
 @memoize
-def f(i, S):
+def f(i, S, K):
+	if S == 0:
+		return 1
 	if i == 1:
 		return min(K, S) + 1
 	r = 0
 	for j in xrange(min(K, S) + 1):
-		r += f(i - 1, S - j)
+		r += f(i - 1, S - j, K)
 		r %= 10 ** 6
 	return r
 
@@ -35,5 +37,8 @@ while True:
 	N, M, K = [int(x) for x in raw_input().split()]
 	if N == 0:
 		break
-	sol = f(N - 1, M - K)
+	for i in xrange(1, N):
+		for k in xrange(M - K + 1):
+			f(i, k, K)
+	sol = f(N - 1, M - K, K)
 	print sol
